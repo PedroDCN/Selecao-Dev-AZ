1e0048a17c71148d0c0b75f8654e2426c28d6544
Pedro D <45217189+PedroDCN@users.noreply.github.com>
2026-06-15 22:25:53 -0300

    Adiciona EmpresaService
    Adiciona Serviço Rest para operações CRUD da entidade Empresa

---
+++ leilao-api/src/main/java/br/com/selecao/locadora/service/EmpresaService.java
@@ -0,0 +1,56 @@
+package br.com.selecao.locadora.service;
+
+import br.com.selecao.locadora.business.EmpresaBO;
+import br.com.selecao.locadora.dto.request.EmpresaRequest;
+import br.com.selecao.locadora.dto.response.EmpresaResponse;
+import br.com.selecao.locadora.entity.Empresa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/empresa")
+public class EmpresaService {
+
+    @Autowired
+    private EmpresaBO empresaBO;
+
+    @GetMapping
+    public ResponseEntity<List<Empresa>> buscarTodos() {
+        return new ResponseEntity<>(empresaBO.buscarTodos(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmpresaResponse> buscarPorId(@PathVariable Long id) {
+        return new ResponseEntity<>(empresaBO.buscarPorId(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/cnpj")
+    public ResponseEntity<EmpresaResponse> buscarPorCnpj(@RequestParam String cnpj) {
+        return new ResponseEntity<>(empresaBO.buscarPorCnpj(cnpj), HttpStatus.OK);
+    }
+
+    @GetMapping("/usuario")
+    public ResponseEntity<EmpresaResponse> buscarPorUsuario(@RequestParam String usuario) {
+        return new ResponseEntity<>(empresaBO.buscarPorUsuario(usuario), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmpresaResponse> salvarEmpresa(EmpresaRequest request) {
+        return new ResponseEntity<>(empresaBO.salvarEmpresa(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmpresaResponse> atualizarEmpresa(@PathVariable("id") Long id, EmpresaRequest request) {
+        return new ResponseEntity<>(empresaBO.atualizarEmpresa(id, request), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<EmpresaResponse> deletarEmpresa(@PathVariable("id") Long id) {
+        empresaBO.deletarEmpresa(id);
+        return ResponseEntity.ok().build();
+    }
+}

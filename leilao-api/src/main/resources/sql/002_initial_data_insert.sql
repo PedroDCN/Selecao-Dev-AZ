f5cf2e93a6e9d0de1ebf422adf28b07bc54569ed
Pedro D <45217189+PedroDCN@users.noreply.github.com>
2026-06-15 21:13:33 -0300

    Adiciona script com massa de dados
    Adiciona o script que realiza a adição da massa de dados de teste

---
+++ leilao-api/src/main/resources/sql/002_initial_data_insert.sql
@@ -0,0 +1,252 @@
+INSERT INTO 
+    empresa (razaoSocial, cnpj, logradouro, municipio, numero, complemento, bairro, telefone, email, site, usuario, senha)
+VALUES
+    ('Ortega e Filhos', '31.256.560/0001-35', 'Rodovia Lauro de Freitas', 'Mossoró', '7130', NULL, 'Loteamento Verdejante', '(66) 92633-6061', 'jacomo.rosa@terra.com.br', 'amaral.net', 'Maraisa', ']nfkvHQ~~CQU?3<'),
+    ('Caldeira e Vale', '36.262.147/0001-05', 'Estrada Doutor José Maria', 'Maracanaú', '7614', NULL, 'Brás', '(92) 97330-6926', 'obatista@franco.org', 'valencia.com', 'Thalita', 'y52|\2(jT<_18^2.CCrC'),
+    ('Escobar e Roque', '13.235.751/0001-39', 'Rodovia Getúlio Vargas', 'Uberaba', '4245', NULL, 'Bela Vista', '(81) 2113-4511', 'sergio.ferraz@pontes.com.br', 'valencia.net.br', 'Isaac', 'xEY>bs9B.$G,zO'),
+    ('Fonseca Comercial Ltda.', '59.327.841/0001-50', 'Alameda do Comércio', 'Laranjal do Jari', '2548', NULL, 'Higienópolis', '(99) 94849-2688', 'vcasanova@hotmail.com', 'duarte.net', 'Cecília', ')Ds::wgW>:6i'),
+    ('Esteves e Galhardo Ltda.', '37.262.621/0001-53', 'Avenida Santos Dumont', 'Corumbá', '7134', NULL, 'Centro', '(35) 90645-0485', 'theo.mascarenhas@torres.org', 'fidalgo.net.br', 'Jonas', 'JA=^Zlk$=-[pmqZKn_6N'),
+    ('Rosa e Filhos', '06.166.871/0001-66', 'Avenida Dom Pedro II', 'Santa Rita', '2305', NULL, 'Jardim Santa Helena', '(96) 99084-9441', 'nero72@gmail.com', 'toledo.com', 'Natália', 'n4nuO#;z6)9Mp5'),
+    ('Caldeira e Godói e Associados', '18.569.388/0001-01', 'Avenida Princesa Isabel', 'Imperatriz', '8420', 'Apt 491', 'Vila Esperança', '(47) 99862-5915', 'lidiane.defreitas@uchoa.com.br', 'ferraz.com.br', 'Edson', ';$3LBUqs%t'),
+    ('Serrano Comercial Ltda.', '48.365.587/0001-69', 'Rodovia Padre Cícero', 'Caracaraí', '7926', NULL, 'Pinheiros', '(47) 2406-6044', 'romero.maraisa@yahoo.com', 'balestero.net', 'Andressa', '#N8WTgDP'),
+    ('Queirós e Colaço S.A.', '39.581.281/0001-59', 'Alameda Marquês de São Vicente', 'Recife', '631A', NULL, 'Conjunto Habitacional', '(91) 3869-8990', 'alice.caldeira@terra.com.br', 'alves.net.br', 'rosa.ana', 'tfG-c%y(sc=a'),
+    ('Lourenço-Estrada', '14.578.918/0001-27', 'Praça Getúlio Vargas', 'Uberaba', '4342', 'Apt 721', 'Boa Vista', '(82) 2327-9832', 'bdasdores@flores.com', 'faro.com.br', 'naomi.leal', 'wdX1GT!8'),
+    ('Fidalgo-Torres', '89.475.260/0001-27', 'Alameda do Comércio', 'Maceió', '1692', NULL, 'Moema', '(11) 98126-3623', 'esalazar@hotmail.com', 'rico.com', 'luis.brito', '-uUkW$u-oxth~}_sj1Q9'),
+    ('Faro Ltda.', '99.138.718/0001-06', 'Estrada Barão do Rio Branco', 'Ilheus', '4688', NULL, 'Vila Olímpia', '(54) 4541-1747', 'vpaes@romero.com', 'campos.br', 'jduarte', 'MRZSnG'),
+    ('Vila e Associados', '34.235.536/0001-08', 'Avenida Padre Cícero', 'Caruaru', '2965', NULL, 'Moema', '(54) 3535-6216', 'reinaldo.torres@valente.br', 'balestero.org', 'maya41', '(BKu4VN5?!L'),
+    ('Tamoio Comercial Ltda.', '67.807.141/0001-91', 'Travessa Sete de Setembro', 'Campina Grande', '7117', 'Apt 47', 'Tatuapé', '(21) 2399-6298', 'hernani.gusmao@perez.com', 'grego.org', 'zguerra', 'VI8]/A(E5n%1IUp,9L'),
+    ('Salazar e Delvalle Ltda.', '85.714.220/0001-30', 'Rodovia do Lavradio', 'Governador Valadares', '3209', 'Apt 210', 'Jardim Paulista', '(61) 3950-5923', 'antonieta21@benites.br', 'quintana.net.br', 'casanova', 'I6b6Tr6Mr4J!Qsl');
+
+INSERT INTO
+    leilao (codigo, descricao, vendedor, inicioPrevisto)
+VALUES
+    (1, 'Leilao de materiais de construcao', 3, '2026-07-01 09:00:00'),
+    (2, 'Leilao de equipamentos industriais', 7, '2026-07-03 10:00:00'),
+    (3, 'Leilao de sucata metalica', 12, '2026-07-05 08:30:00'),
+    (4, 'Leilao de madeira beneficiada', 5, '2026-07-08 14:00:00'),
+    (5, 'Leilao de material eletrico', 8, '2026-07-10 09:30:00'),
+    (6, 'Leilao de ferragens', 1, '2026-07-12 10:00:00'),
+    (7, 'Leilao de tubos PVC', 10, '2026-07-14 08:00:00'),
+    (8, 'Leilao de materiais hidraulicos', 6, '2026-07-15 15:00:00'),
+    (9, 'Leilao de ferramentas', 4, '2026-07-18 09:00:00'),
+    (10, 'Leilao de cimento e agregados', 9, '2026-07-20 10:00:00'),
+    (11, 'Leilao de equipamentos de escritorio', 14, '2026-07-22 13:00:00'),
+    (12, 'Leilao de moveis corporativos', 11, '2026-07-24 09:00:00'),
+    (13, 'Leilao de materiais escolares', 15, '2026-07-25 14:30:00'),
+    (14, 'Leilao de pecas automotivas', 2, '2026-07-27 08:30:00'),
+    (15, 'Leilao de pneus', 13, '2026-07-29 10:00:00'),
+    (16, 'Leilao de computadores', 5, '2026-08-01 09:00:00'),
+    (17, 'Leilao de equipamentos hospitalares', 8, '2026-08-03 11:00:00'),
+    (18, 'Leilao de produtos agricolas', 6, '2026-08-05 08:00:00'),
+    (19, 'Leilao de materiais diversos', 1, '2026-08-08 14:00:00'),
+    (20, 'Leilao de equipamentos de seguranca', 10, '2026-08-10 09:00:00');
+
+INSERT INTO 
+    unidade (nome)
+VALUES
+    ('NAO_CONHECIDA'),
+    ('KG'),
+    ('TON'),
+    ('LITRO'),
+    ('METRO'),
+    ('METRO_QUADRADO'),
+    ('METRO_CUBICO'),
+    ('CAIXA'),
+    ('PACOTE'),
+    ('UNIDADE'),
+    ('SACA');
+
+INSERT INTO
+    lote (numeroLote, descricao, quantidade, valorInicial, unidade, leilao)
+VALUES
+    (1,'Cimento CP-II',500,25000,11,1),
+    (2,'Areia fina',120,18000,7,1),
+    (3,'Brita 01',80,16000,7,1),
+    (4,'Vergalhao 10mm',3000,45000,2,1),
+    (5,'Tijolo ceramico',10000,30000,10,1),
+
+    (1,'Motor eletrico 10CV',4,12000,10,2),
+    (2,'Motor eletrico 20CV',2,15000,10,2),
+    (3,'Compressor industrial',1,18000,10,2),
+    (4,'Gerador diesel',2,25000,10,2),
+    (5,'Painel eletrico',6,9000,10,2),
+
+    (1,'Sucata de ferro',15,22000,3,3),
+    (2,'Sucata de aluminio',8,18000,3,3),
+    (3,'Sucata de cobre',4,32000,3,3),
+    (4,'Sucata mista',10,15000,3,3),
+    (5,'Cabos inutilizados',2,5000,3,3),
+
+    (1,'Tabua pinus',500,12000,5,4),
+    (2,'Viga madeira',200,18000,5,4),
+    (3,'Compensado naval',300,15000,6,4),
+    (4,'Porta madeira',100,10000,10,4),
+    (5,'Janela madeira',80,9000,10,4),
+
+    (1,'Fio 2.5mm',5000,12000,5,5),
+    (2,'Fio 6mm',3000,15000,5,5),
+    (3,'Disjuntor',200,8000,10,5),
+    (4,'Tomada',1000,5000,10,5),
+    (5,'Interruptor',900,4500,10,5),
+
+    (1,'Parafuso sextavado',5000,3500,10,6),
+    (2,'Porca sextavada',5000,2500,10,6),
+    (3,'Arruela lisa',8000,2000,10,6),
+    (4,'Chumbador',1000,4000,10,6),
+    (5,'Barra roscada',400,6000,5,6),
+
+    (1,'Tubo PVC 25mm',2000,9000,5,7),
+    (2,'Tubo PVC 50mm',1500,12000,5,7),
+    (3,'Conexao PVC',800,3500,10,7),
+    (4,'Joelho PVC',700,2500,10,7),
+    (5,'Registro PVC',200,5000,10,7),
+
+    (1,'Caixa dagua 1000L',30,15000,10,8),
+    (2,'Bomba hidraulica',15,22000,10,8),
+    (3,'Torneira metalica',300,6000,10,8),
+    (4,'Valvula pressao',120,5000,10,8),
+    (5,'Mangueira industrial',800,10000,5,8),
+
+    (1,'Marreta',100,3000,10,9),
+    (2,'Martelo',300,3500,10,9),
+    (3,'Chave inglesa',150,4500,10,9),
+    (4,'Alicate universal',200,5000,10,9),
+    (5,'Furadeira',50,15000,10,9),
+
+    (1,'Cimento CP-IV',800,40000,11,10),
+    (2,'Brita 02',100,20000,7,10),
+    (3,'Areia grossa',150,25000,7,10),
+    (4,'Pedrisco',70,14000,7,10),
+    (5,'Cal hidratada',300,9000,11,10),
+
+    (1,'Mesa escritorio',50,12000,10,11),
+    (2,'Cadeira giratoria',120,18000,10,11),
+    (3,'Armario arquivo',40,10000,10,11),
+    (4,'Monitor LCD',70,14000,10,11),
+    (5,'Impressora',20,9000,10,11),
+
+    (1,'Mesa reuniao',10,8000,10,12),
+    (2,'Sofa recepcao',8,6000,10,12),
+    (3,'Estante metalica',40,7000,10,12),
+    (4,'Balcao atendimento',12,5000,10,12),
+    (5,'Arquivo deslizante',6,15000,10,12),
+
+    (1,'Caderno universitario',3000,6000,10,13),
+    (2,'Caneta esferografica',10000,5000,10,13),
+    (3,'Lapiseira',2000,3000,10,13),
+    (4,'Borracha escolar',5000,2000,10,13),
+    (5,'Mochila escolar',500,12000,10,13),
+
+    (1,'Pneu aro 13',200,15000,10,14),
+    (2,'Pneu aro 14',180,18000,10,14),
+    (3,'Pneu aro 15',150,20000,10,14),
+    (4,'Pneu aro 16',120,22000,10,14),
+    (5,'Pneu aro 17',100,25000,10,14),
+
+    (1,'Amortecedor',300,18000,10,15),
+    (2,'Radiador',100,14000,10,15),
+    (3,'Farol automotivo',500,12000,10,15),
+    (4,'Retrovisor',400,9000,10,15),
+    (5,'Parachoque',80,11000,10,15),
+
+    (1,'Notebook',40,50000,10,16),
+    (2,'Desktop',50,45000,10,16),
+    (3,'Monitor LED',80,20000,10,16),
+    (4,'Switch rede',30,12000,10,16),
+    (5,'Servidor rack',5,80000,10,16),
+
+    (1,'Maca hospitalar',25,30000,10,17),
+    (2,'Monitor cardiaco',10,70000,10,17),
+    (3,'Cadeira rodas',40,18000,10,17),
+    (4,'Respirador',8,90000,10,17),
+    (5,'Bomba infusao',20,25000,10,17),
+
+    (1,'Milho',500,35000,11,18),
+    (2,'Soja',700,50000,11,18),
+    (3,'Feijao',300,22000,11,18),
+    (4,'Trigo',400,28000,11,18),
+    (5,'Algodao',120,45000,11,18),
+
+    (1,'Material diverso A',100,4000,8,19),
+    (2,'Material diverso B',120,4500,8,19),
+    (3,'Material diverso C',140,5000,8,19),
+    (4,'Material diverso D',160,5500,8,19),
+    (5,'Material diverso E',180,6000,8,19),
+
+    (1,'Capacete seguranca',500,12000,10,20),
+    (2,'Luva protecao',2000,8000,10,20),
+    (3,'Oculos seguranca',1000,7000,10,20),
+    (4,'Colete refletivo',600,9000,10,20),
+    (5,'Botina seguranca',800,18000,10,20);
+
+INSERT INTO
+    comprador (empresa, leilao)
+VALUES
+    (2, 1),
+    (5, 1),
+    (8, 1),
+
+    (1, 2),
+    (4, 2),
+    (9, 2),
+
+    (3, 3),
+    (7, 3),
+
+    (6, 4),
+    (11, 4),
+
+    (1, 5),
+    (3, 5),
+    (6, 5),
+    (8, 5),
+    (11, 5),
+    (14, 5),
+
+    (4, 6),
+    (15, 6),
+
+    (1, 7),
+    (10, 7),
+
+    (3, 8),
+    (12, 8),
+
+    (5, 9),
+    (14, 9),
+
+    (2, 10),
+    (7, 10),
+    (11, 10),
+
+    (6, 11),
+    (15, 11),
+
+    (2, 12),
+    (5, 12),
+    (9, 12),
+    (13, 12),
+
+    (1, 13),
+    (9, 13),
+
+    (3, 14),
+    (10, 14),
+
+    (5, 15),
+    (12, 15),
+
+    (2, 16),
+    (14, 16),
+
+    (6, 17),
+    (11, 17),
+
+    (4, 18),
+    (13, 18),
+
+    (7, 19),
+    (15, 19),
+
+    (8, 20),
+    (10, 20);

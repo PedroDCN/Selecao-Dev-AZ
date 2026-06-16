10a717ff45760daa79312a255b77101f9f16e5f1
Pedro D <45217189+PedroDCN@users.noreply.github.com>
2026-06-15 22:25:03 -0300

    Adiciona UnidadeService
    Adiciona Serviço Rest para operações CRUD da entidade Unidade

--- leilao-api/src/main/java/br/com/selecao/locadora/service/UnidadeService.java
+++ leilao-api/src/main/java/br/com/selecao/locadora/service/UnidadeService.java
@@ -1,22 +1,46 @@
 package br.com.selecao.locadora.service;
 
 import br.com.selecao.locadora.business.UnidadeBO;
+import br.com.selecao.locadora.dto.request.UnidadeRequest;
+import br.com.selecao.locadora.dto.response.UnidadeResponse;
+import br.com.selecao.locadora.entity.Unidade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/unidades")
+@RequestMapping(value = "/unidade")
 public class UnidadeService {
 
     @Autowired
     private UnidadeBO unidadeBO;
 
     @GetMapping
-    public ResponseEntity<Object> buscarTodos() {
+    public ResponseEntity<List<Unidade>> buscarTodos() {
         return new ResponseEntity<>(unidadeBO.buscarTodos(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UnidadeResponse> buscarPorId(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(unidadeBO.buscarPorId(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<UnidadeResponse> salvarUnidade(@RequestBody UnidadeRequest request) {
+        return new ResponseEntity<>(unidadeBO.salvarUnidade(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UnidadeResponse> atualizarUnidade(@PathVariable("id") Long id, @RequestBody UnidadeRequest request) {
+        return new ResponseEntity<>(unidadeBO.atualizarUnidade(id, request), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarUnidade(@PathVariable Long id) {
+        unidadeBO.deletarUnidade(id);
+        return ResponseEntity.ok().build();
     }
 }

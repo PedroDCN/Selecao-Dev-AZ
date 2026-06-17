daf30e9612e2e2927f4fa1d1f3518dd2d20e0105
Pedro D <45217189+PedroDCN@users.noreply.github.com>
2026-06-17 16:30:51 -0300

    Captura erro de Integridade do Banco
    Adiciona tratamento de erro para erros de integridade do banco de dados, retornando mensagem personalizada específica ao invés do genérico erro 500

--- leilao-api/src/main/java/br/com/selecao/locadora/handler/GlobalExceptionHandler.java
+++ leilao-api/src/main/java/br/com/selecao/locadora/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package br.com.selecao.locadora.handler;
 
 import br.com.selecao.locadora.exception.*;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +10,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        Throwable cause = ex.getCause();
+        if (cause instanceof ConstraintViolationException) {
+            String constrainName = ((ConstraintViolationException) cause).getConstraintName();
+            String specificCause = handleSpecificConstrain(constrainName);
+            return new ResponseEntity<>(specificCause, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    private String handleSpecificConstrain(String constrainName) {
+        if (constrainName == null) return "Erro no banco de dados desconhecido";
+        switch (constrainName) {
+            case "empresa_leilao_fk":
+                return "Não pode deletar empresa: empresa possui leilões ativos";
+            case "empresa_comp_fk":
+                return "Não pode deletar empresa: empresa participa de leilões";
+            case "leilao_comp_fk":
+                return "Não pode deletar leilão: empresa(s) participa(m) de leilão";
+            case "leilao_lote_fk":
+                return "Não pode deletar leilão: leilão possui lotes";
+            case "unidade_lote_fk":
+                return "Não pode deletar lote: unidade sendo usada por lote";
+            default:
+                return "Erro no banco de dados desconhecido: " + constrainName;
+        }
+    }
 
     @ExceptionHandler(CompradorInvalidoException.class)
     public ResponseEntity<String> handleCompradorInvalidoException(CompradorInvalidoException ex) {
